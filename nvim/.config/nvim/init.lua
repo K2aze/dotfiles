43c01a3c74897333cae50328d5b9f707f01bb98e
laptop <killallstart@gmail.com>
2025-10-03 17:47:06 +0900

    itinial commit

---
+++ nvim/.config/nvim/init.lua
@@ -0,0 +1,16 @@
+
+
+--GENERAL SETTINGS--
+vim.g.mapleader = "="
+
+vim.opt.number = true
+vim.opt.shiftwidth = 2
+vim.opt.mouse = a
+vim.opt.expandtab = true
+
+
+
+require('plugins')
+require('colorscheme')
+require('explorer')
+require('terminal')
---
+++ nvim/.config/nvim/lua/terminal.lua
@@ -0,0 +1,13 @@
+vim.keymap.set(
+  "n",
+  "<leader>ter",
+  "<cmd>FloatermToggle<CR>",
+  { noremap = true, silent = true, desc = "Toggle Floaterm" }
+)
+
+vim.keymap.set(
+  "t",
+  "<leader>ter",
+  "<cmd>FloatermToggle<CR>",
+  { noremap = true, silent = true, desc = "Toggle Floaterm" }
+)
