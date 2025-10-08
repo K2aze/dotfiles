d405adefdc6b6c3e8637e8e3683f1a8c2f84a477
K2aze Laptop <Killallstart@gmail.com>
2025-10-08 21:11:23 +0900

    update tree

---
+++ nvim/lua/config/tree.lua
@@ -0,0 +1,31 @@
+require('nvim-treesitter').setup({
+  highlight = {
+    enable = true,
+    disable = {},
+  },
+  indent = {
+    enable = true,
+    disable = {},
+  },
+  ensure_installed = {
+    "tsx",
+    "toml",
+    "fish",
+    "php",
+    "json",
+    "yaml",
+    "swift",
+    "css",
+    "html",
+    "lua"
+  },
+  autotag = {
+    enable = true,
+  },
+})
+
+
+--local parser_config = require "nvim-treesitter.parsers".get_parser_configs()
+--parser_config.tsx.filetype_to_parsername = { "javascript", "typescript.tsx" }
+
+require("nvim-ts-autotag").setup({})
