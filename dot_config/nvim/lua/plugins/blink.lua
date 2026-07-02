81f96dcb9ad37cf7f4d1fe90ba69e97f3be43cb2
archlix@k2aze <killallstart@gmail.com>
2026-07-02 11:36:59 +0900

    change: add blink.lib for blink.cmp / remove unnecessary files

--- dot_config/nvim/lua/plugins/blink.lua
+++ dot_config/nvim/lua/plugins/blink.lua
@@ -1,9 +1,4 @@
-vim.pack.add({
-  {
-    src = "https://github.com/saghen/blink.cmp",
-    version = vim.version.range("^1"),
-  },
-})
+vim.pack.add({ 'https://github.com/saghen/blink.lib', 'https://github.com/saghen/blink.cmp' })
 
 -- Lazy load on first insert mode entry
 local group = vim.api.nvim_create_augroup("BlinkCmpLazyLoad", { clear = true })
